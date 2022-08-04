71a1b6c618c1fcda0533c37eda9674080e91ea41
jan1ooo <filhojanio67@gmail.com>
2022-08-04 18:31:36 -0300

    Cadastro de participante, conexao com kafka e gravacao do voto

--- src/main/java/me/dio/coding/votacao/bbb/api/repository/ParticipanteRepository.java
+++
@@ -1,8 +0,0 @@
-package me.dio.coding.votacao.bbb.api.repository;
-
-import me.dio.coding.votacao.bbb.api.model.ParticipanteModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ParticipanteRepository extends MongoRepository<ParticipanteModel, String> {
-
-}
